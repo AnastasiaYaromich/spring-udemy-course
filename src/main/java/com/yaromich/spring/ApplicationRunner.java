89ae62f82d30d1d8d6ca357c09ad1a1062cff8f0
JYaromich <jane19937788@gmail.com>
2023-11-26 17:35:19 +0300

    xml-based configuration

--- src/main/java/com/yaromich/spring/ApplicationRunner.java
+++ src/main/java/com/yaromich/spring/ApplicationRunner.java
@@ -7,10 +7,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class ApplicationRunner {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
-        ConnectionPool connectionPool = context.getBean("p1", ConnectionPool.class);
+        ConnectionPool connectionPool = context.getBean("pool1", ConnectionPool.class);
         System.out.println(connectionPool);
         CompanyRepository companyRepository = context.getBean("companyRepository", CompanyRepository.class);
         System.out.println(companyRepository);
-        context.close();
     }
 }
--- src/main/java/com/yaromich/spring/repository/pool/ConnectionPool.java
+++ src/main/java/com/yaromich/spring/repository/pool/ConnectionPool.java
@@ -1,13 +1,13 @@
 package com.yaromich.spring.repository.pool;
 
-import org.springframework.beans.factory.InitializingBean;
 import java.util.*;
 
-public class ConnectionPool implements InitializingBean {
-    private final String username;
-    private final Integer poolSize;
-    private final List<Object> args;
+public class ConnectionPool {
+    private String username;
+    private Integer poolSize;
+    private List<Object> args;
     private Map<String, Object> properties;
+
 
     public ConnectionPool(String username, Integer poolSize, List<Object> args, Map<String, Object> properties) {
         this.username = username;
@@ -18,18 +18,5 @@ public class ConnectionPool implements InitializingBean {
 
     public void setProperties(Map<String, Object> properties) {
         this.properties = properties;
-    }
-
-    private void init() {
-        System.out.println("Initialization connection pool");
-    }
-
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("Properties set");
-    }
-
-    private void destroy() {
-        System.out.println("Clean connection pool");
     }
 }
